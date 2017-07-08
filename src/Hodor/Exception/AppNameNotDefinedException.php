cbd78439719786cef4a9bdb695d3a0344668df5f
lovelock <frostwong@gmail.com>
2017-07-08 14:57:14 +0800

    throw exception when APP_NAME is not defined

---
+++ src/Hodor/Exception/AppNameNotDefinedException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frost Wong <frostwong@gmail.com>
+ * Date: 08/07/2017
+ * Time: 14:56
+ */
+
+namespace Hodor\Exception;
+
+
+use Exception;
+
+class AppNameNotDefinedException extends Exception
+{
+
+}
